36787701a06dd2733256c3e956d5933e5a62ba29
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2020-06-14 16:11:02 +0200

    Hasher les MDP ! Comment rendre nos mots de passe « inviolables »

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,16 +10,18 @@ use Doctrine\Persistence\ObjectManager;
 use Doctrine\ORM\EntityManagerInterface; // A la place de ObjectManager car ne fonctionnait pas.
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(Request $request, EntityManagerInterface $manager) // La fonctionne registration à besoin de : 
+    public function registration(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder) 
+                                    // La fonctionne registration à besoin de : 
                                     // La requête HTTP => pour pouvoir l'analyser, pour que notre formulaire en tire les informations qui sont ressorti.
                                     // L'objectManager de doctrine => qui va permettre d'enregistrer l'utilisateur en BDD.
-    {
+    {                               // UserPasswordEncoder => Interface qui permet d’encoder les mots de passes !
         // Dire à quel Objet relier les champs du formulaire.
         $user = new User();
 
@@ -32,6 +34,13 @@ class SecurityController extends AbstractController
         // Si le formulaire est soumis et que tout ses champs sont valide
         if($form->isSubmitted() && $form->isValid() )
         {
+            // Avant de sauvegarder mon utilisateur, j'ai envie de dire : je veux ici un hash et ça sera égale à mon encodeur à qui je vais demander d'encoder un password encodePassword()
+            // Mais la cet encodeur peut encoder en pleins d'algorithme différent. Je dois donc lui faire comprendre quel est l'algorithme qui m'intéresse. Je lui passe donc mon $user car $user est une instance de la classe EntityUser et dans le security.yaml j'ai dit quand on tombe sur User il faut utiliser l'algorithme bcrypt
+            $hash = $encoder->encodePassword($user, $user->getPassword()); // Je lui donne le mdp que je veux encoder. Il est dans $user->getPassword() | J'ai du coup dans mon hash, le bon hash.
+
+            // J'ai mon mdp encoder dans $hash donc je peux dire : Je modifie ton pswd et à la place je te met le hash.
+            $user->setPassword($hash);
+
             // Alors je veux que tu fasse persister dans le temps le $user dans la BDD.
             $manager->persist($user);
             $manager->flush();
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,12 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 Use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User implements UserInterface // UserInterface <=> L’interface qu’on doit implémenter si on veut créer des users
 {
     /**
      * @ORM\Id()
@@ -79,4 +80,15 @@ class User
 
         return $this;
     }
+
+
+    public function eraseCredentials(){}    // Ne fera rien
+
+    public function getSalt(){}  // Ne fera rien aussi pour l'instant vu qu'on a rien de spécial.
+
+    public function getRoles(){   // Doit renvoyer un tableau de chaine de caractère qui explique quel est le rôle de cet utilisateur. Pour l'instant vu qu'on a pas encore fait de gestion de rôles on va simplement dire que c'est un tableau qui va contenir un utilisateur classic. Il n'y a pas encore d'administration etc donc c'est juste un role_user. A terme ici, quand on aura plusieurs rôles il faudra ici que getRoles envoie les rôles qui sont au sein de cet utilisateur, qui sont donné à cet utilisateur.
+        
+    return ['ROLE_USER'];
+    }
+
 }
